a37ef89798a6b05d4b0f78adaac137c1d9124a10
Miguel <migant1306@gmail.com>
2023-03-07 17:18:36 -0300

    modified button position

--- src/components/GamesCard/index.tsx
+++ src/components/GamesCard/index.tsx
@@ -1,5 +1,11 @@
 interface GameProps {
-  game: string;
+  game: {
+    id: string;
+    name: string;
+    background_image: string;
+    slug: string;
+    rating: number;
+  };
   showLink: boolean;
 }
 
@@ -11,15 +17,20 @@ import "./style.css"
 
 function GamesCard({ game, showLink = true }: GameProps) {
   return (
-      <div className="game">
-        <img src={game.background_image} alt={game.slug} id={game.id} />
-        <h2>{game.name}</h2>
-        <span>
-          <FaStar /> {game.rating}
-        </span>
-        <br />
-        {showLink && <Link to={`/game/${game.id}/${game.slug}`}>Ver Mais</Link>}
+    <div className="game">
+      <div>
+        <div className="img">
+          <img src={game.background_image} alt={game.slug} id={game.id} />
+        </div>
+        <div className='h2-sapn'>
+          <h2>{game.name}</h2>
+          <span>
+            <FaStar /> {game.rating}
+          </span>
+        </div>
       </div>
+        {showLink && <Link to={`/game/${game.id}/${game.slug}`}>Ver Mais</Link>}
+    </div>
   )
 }
 
